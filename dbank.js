cc3a34874197c455a532e001fe9a4ddbd9b5e023
michaelhasler <michael@mhasler.eu>
2022-02-28 13:09:28 +0100

    Fixing bug, of depositing from wrong account.

--- dbank.js
+++ dbank.js
@@ -6,14 +6,19 @@ app.listen(port, function() {
     console.log(`Example app listening on port ${port}!`)
 });
 
+// -- Webserver started
+
+// Datenbank
 const accounts = {
     'a': 200000
 };
 
+// How much balance on :account ?
 app.get('/balance/:account', (req, res) => {
     res.send("Balance: " + accounts[req.params.account] || 0);
 });
 
+// Transfer from :from to :to with amount :amount
 app.post('/transfer/:from/:to/:amount', (req, res) => {
     const amount = parseInt(req.params.amount);
 
@@ -21,11 +26,12 @@ app.post('/transfer/:from/:to/:amount', (req, res) => {
         return res.status(400).send("Insufficent funds");
 
     accounts[req.params.from] -= amount;
-    accounts[req.params.to] = (accounts[req.params.from] || 0) + amount;
+    accounts[req.params.to] = (accounts[req.params.to] || 0) + amount;
 
     return res.send(true);
 });
 
+// Get database
 app.get('/balances', (req, res) => {
     return res.send(accounts);
 })
