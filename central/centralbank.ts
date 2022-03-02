ef37c32d4851ba9a0fdd8650911657d1f161fe94
Klaiberm <klaiberm@dhbw-loerrach.de>
2022-03-02 20:54:26 +0100

    Implement, improve and reorganise centralised banking

---
+++ central/centralbank.ts
@@ -0,0 +1,52 @@
+import * as fs from 'fs';
+const express = require('express');
+const app = express();
+
+const database = 'central/database.json';
+const port = 3000;
+
+// Webserver started
+app.listen(port, function() {
+    console.log(
+        `Example app listening at address http://localhost:${port}!\n`,
+        { accounts: accounts }
+    );
+});
+
+if (!fs.existsSync(database)) {
+    fs.writeFileSync(database, JSON.stringify({ 'central': 20_000 }));
+}
+const accounts = JSON.parse(fs.readFileSync(database).toString());
+
+// How much balance on :account ?
+app.get('/balance/:account', (req, res) => {
+    res.send('Balance: ' + accounts[req.params.account] || 0);
+});
+
+// Transfer from :from to :to with amount :amount
+app.post('/transfer/:from/:to/:amount', (req, res) => {
+    const amount = parseInt(req.params.amount);
+    if (amount < 0)
+        return res.status(400).send('Rejected - negative payment');
+    if (accounts[req.params.from] < amount)
+        return res.status(400).send('Rejected - insufficient funds');
+
+    accounts[req.params.from] -= amount;
+    accounts[req.params.to] = (accounts[req.params.to] || 0) + amount;
+
+    // store in database
+    fs.writeFileSync(database, JSON.stringify(accounts));
+
+    return res.send(true);
+});
+
+// Get database
+app.get('/balances', (req, res) => {
+    return res.send(accounts);
+})
+
+// Get Version
+app.get('/version', (req, res) => {
+    const package_ = require('../package.json');
+    return res.send('Version: ' + package_.version);
+});
---
+++ central/dbank.ts
@@ -0,0 +1,71 @@
+const express = require('express');
+const app = express();
+const fetch_ = require('node-fetch');
+
+const url = `http://65.21.147.130:5000`
+const bank_id = 'a';
+const port = 3000;
+
+const getCurrentBalances = async(bank_id) => {
+    const payload = await fetch_(`${url}/balances`);
+    return payload.json();
+}
+
+// Webserver started
+app.listen(port, async function() {
+    console.log(
+        `Example app listening at address http://localhost:${port}!\n`,
+        { accounts: await getCurrentBalances(bank_id) }
+    );
+});
+
+/** How much balance on :account ?
+ * @example curl http://localhost:3000/balance/a_a
+ */
+app.get('/balance/:account', async (req, res) => {
+    const accounts = await getCurrentBalances(bank_id);
+    return res.send('Balance: ' + accounts[req.params.account]);
+});
+
+/** Transfer from :from to :to with amount :amount
+ * @example curl -X POST http://localhost:3000/transfer/a_a/a_b/1000
+ */
+app.post('/transfer/:from/:to/:amount', async (req, res) => {
+    const amount = parseInt(req.params.amount);
+    if (amount < 0)
+        return res.status(400).send('Rejected - negative payment');
+
+    const accounts = await getCurrentBalances(bank_id);
+    const from = req.params.from;
+    const to = req.params.to;
+
+    if (accounts[from] < amount)
+        return res.status(400).send('Rejected - insufficient funds');
+
+    await fetch_(
+        `${url}/transfer/${from}/${to}/${amount}`,
+    {
+            method: 'post',
+            body: JSON.stringify({}),
+            headers: {'Content-Type': 'application/json'}
+        }
+    );
+
+    return res.send(true);
+});
+
+/** Get database
+ * @example curl http://localhost:3000/balances
+ */
+app.get('/balances', async(req, res) => {
+    const accounts = await getCurrentBalances(bank_id);
+    return res.json(accounts);
+})
+
+/** Get version
+ * @example curl http://localhost:3000/version
+ */
+app.get('/version', (req, res) => {
+    const package_ = require('../package.json');
+    return res.send('Version: ' + package_.version);
+});
