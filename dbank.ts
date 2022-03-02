6d1711ed8bf927d7711c35b0d39e604c53d01869
Klaiberm <klaiberm@dhbw-loerrach.de>
2022-03-02 12:21:10 +0100

    db as file, added eslint

--- dbank.ts
+++ dbank.ts
@@ -1,3 +1,5 @@
+import * as fs from "fs";
+
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -9,11 +11,18 @@ app.listen(port, function() {
 // -- Webserver started
 
 // Datenbank
-const accounts = {
-    'a': 200_000,
-    'b': 15_000,
-    'c': 100_000
-};
+let accounts = null;
+if (fs.existsSync("./dbase.json")) {
+    // @ts-ignore
+    accounts = JSON.parse(fs.readFileSync("./dbase.json"));
+} else {
+    accounts = {
+        'a': 10_000,
+        'b': 20_000,
+        'c': 30_000,
+    };
+    fs.writeFileSync("./dbase.json", JSON.stringify(accounts));
+}
 
 // How much balance on :account ?
 app.get('/balance/:account', (req, res) => {
@@ -30,6 +39,9 @@ app.post('/transfer/:from/:to/:amount', (req, res) => {
     accounts[req.params.from] -= amount;
     accounts[req.params.to] = (accounts[req.params.to] || 0) + amount;
 
+    // store in database
+    fs.writeFileSync("./dbase.json", JSON.stringify(accounts));
+
     return res.send(true);
 });
 
@@ -40,7 +52,7 @@ app.get('/balances', (req, res) => {
 
 // Get Version
 app.get('/version', (req, res) => {
-    const package = require("./package.json");
-    return res.send("Version: " + package.version);
+    const package_ = require("./package.json");
+    return res.send("Version: " + package_.version);
 });
 
